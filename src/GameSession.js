2b34c3c3815c4d7730318878664d261737c14e19
Andreas Ambühl <36681000+AndiSwiss@users.noreply.github.com>
2019-11-03 00:44:57 +0100

    comment deleted

--- src/GameSession.js
+++ src/GameSession.js
@@ -48,7 +48,6 @@ export default class GameSession {
     // for testing -> select a specific game -> work in progress
     // const minigame = this.minigames[this.minigames.length]
 
-    // this.state = new GameStates.StateMinigame(minigame)
     this.state = new GameStates.StateMiniGame(minigame)
   }
 
