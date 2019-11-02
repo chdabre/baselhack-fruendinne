db0b8934ee2f2c49a5d3085de51dbc751044efa2
Dario Breitenstein <online@imakethings.ch>
2019-11-02 13:16:02 +0100

    add vueSocketIo

--- host/src/main.js
+++ host/src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: typeof webpackHotUpdate !== 'undefined' ? `http://localhost:3000` : `http://${window.location.host}`,
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
 
 Vue.config.productionTip = false
 
