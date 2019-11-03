a92a939bbaa05a9026bfad20768e618200e77bed
Philipp Alder <philipp.alder@students.fhnw.ch>
2019-11-03 15:30:17 +0100

    game almost complete and styled

--- minigames/time-guessing/index.js
+++ minigames/time-guessing/index.js
@@ -1,57 +1,32 @@
-const button = document.getElementById('btn')
-
-
-//Neu
-const displayText = document.getElementById('dispText')
-// const buttonT = document.getElementById('btnTime')
+const button = document.getElementById('btn');
+const displayText = document.getElementById('dispText');
 var startTime;
 var stopTime;
 var diffTime;
-var tempScore
+var tempScore;
+var validScores = false;
 
+const params = new URLSearchParams(window.location.search);
+const playerId = params.get('playerId');
+const players = JSON.parse(params.get('players')); //array of all player
 
-const params = new URLSearchParams(window.location.search)
-const playerId = params.get('playerId')
-
-let gameEnded = false
+let gameEnded = false;
 let playerScores = {}
-playerScores[playerId] = 0
-
-//neu
-let readyPlayers = {}
+playerScores[playerId] = 0;
+let readyPlayers = {};
 let mainExecuted = false;
-
-//neu
 let readyState = false;
 
 
-// button.addEventListener('click', event => {
-//   playerScores[playerId]++
-//   gameEl.innerText = playerScores[playerId]
-//
-//   parent.postMessage({
-//     source: 'minigame',
-//     playerId,
-//     score: playerScores[playerId]
-//   }, 'http://localhost:8081')
-// })
-
-
-//Neu
 button.addEventListener('click', event => {
 
   button.style.visibility = 'hidden';
-  //Neu
   stopTime = new Date().getTime()
   diffTime = stopTime - startTime
 
   tempScore = Math.abs(60000 - diffTime)
-
-
-  playerScores[playerId]=(180000-tempScore);
-
-    console.log("tempScore = " + tempScore)
-
+  playerScores[playerId] = (180000 - tempScore);
+  console.log("tempScore = " + tempScore)
 
   parent.postMessage({
     source: 'minigame',
@@ -59,7 +34,7 @@ button.addEventListener('click', event => {
     score: playerScores[playerId]
   }, 'http://localhost:8081')
 
-})
+});
 
 window.addEventListener('message', msg => {
   if (msg.data.source === 'minigame') {
@@ -68,60 +43,82 @@ window.addEventListener('message', msg => {
     }
     testWin()
   }
-})
+});
 
-function testWin () {
-  Object.keys(playerScores).forEach(key => {
-    if (playerScores[key] >= 10) {
-      // Someone has won.
-      if (key === playerId) {
-        // I have won.
-        document.getElementById('you_win').style.display = 'block'
-      } else {
-        // Someone else has won.
-        document.getElementById('you_lose').style.display = 'block'
-      }
 
-      // I am the master player and must report the results
-      if (playerId === '0') {
-        sendWinSignal(playerScores)
+function testWin() {
+
+  if (Object.keys(playerScores).length === players.length)
+    validScores = true;
+
+  if (validScores) {
+    let winner = true;
+    Object.values(playerScores).forEach(val => {
+      if (val < tempScore) {
+        winner = false;
       }
+    });
+
+    // Someone has won.
+    if (winner) {
+      // I have won.
+      document.getElementById('you_win').style.display = 'block'
+    } else {
+      // Someone else has won.
+      document.getElementById('you_lose').style.display = 'block'
     }
-  })
-}
-
-function sendWinSignal (playerScores) {
-  if (!gameEnded) {
-    setTimeout(() => {
-      parent.postMessage({
-        source: 'minigame',
-        event: 'win',
-        playerScores
-      }, 'http://localhost:8081')
-
-      gameEnded = true
-    }, 1000)
+    // I am the master player and must report the results
+    if (playerId === '0') {
+      sendWinSignal(playerScores)
+    }
   }
 }
 
-function testStart(data){
+function sendWinSignal(playerScores) {
+  let ranking = Object.keys(playerScores).sort((a, b) => {
+    return playerScores[a] - playerScores[b];
+  })
+
+  //ranking = Object.keys(playerScores)
+  console.log(Object.keys(playerScores).sort((a, b) => {
+    return playerScores[a] - playerScores[b];
+  }))
+
+  let points = Array.from(ranking);
+  for (let i = 0; i < ranking.length; i++) {
+    points[i] = playerScores[ranking[i]];
+  }
+
+  let winReturn = {ranking, points};
+
+  console.log("WIN RETURN:")
+  console.log(winReturn)
+
+  if (!gameEnded) {
+    parent.postMessage({
+      source: 'minigame',
+      event: 'win',
+      playerScores: winReturn,
+    }, 'http://localhost:8081')
+
+    gameEnded = true
+  }
+}
+
+function testStart(data) {
   //console.log("testing ready")
   readyPlayers[data.playerId] = data.ready;
-  for(player in readyPlayers){
-    if(!player) return false
+  for (player in readyPlayers) {
+    if (!player) return false
   }
   return true
 }
 
 setup();
 
-function setup(){
-
-  //for testing because there are no ready players. needs to be moved to the end of setup later
-  main()
-
+function setup() {
   //setup readyPlayers Object
-  for(player in players){
+  for (player in players) {
     readyPlayers[player] = false;
   }
 
@@ -131,10 +128,10 @@ function setup(){
         playerScores[msg.data.playerId] = msg.data.score
 
       }
-      if(testStart(msg.data)){
+      if (testStart(msg.data)) {
         readyState = true;
         console.log("all ready")
-        if(!mainExecuted) main();
+        if (!mainExecuted) main();
         if (!readyState) {
           console.log("not all clients are ready!")
         }
@@ -142,7 +139,7 @@ function setup(){
       console.log(playerScores)
       testWin()
     }
-  })
+  });
 
   parent.postMessage({
     source: 'minigame',
@@ -151,36 +148,37 @@ function setup(){
     ready: true,
   }, 'http://localhost:8081')
 
+  //needs to move to beginning of setup for testing the game
+  main()
+
 }
 
 
-function main(){
-  //mainExecuted = true;
+function main() {
+  mainExecuted = true;
   startTime = new Date().getTime();
-
 
   button.style.visibility = 'hidden';
   displayText.innerText = "3"
 
-  setTimeout(function(){
+  setTimeout(function () {
     displayText.innerText = "2"
   }, 2000);
 
-  setTimeout(function(){
+  setTimeout(function () {
     displayText.innerText = "1"
   }, 3000);
 
-  setTimeout(function(){
+  setTimeout(function () {
     displayText.innerText = "GO!"
   }, 4000);
 
-  setTimeout(function(){
+  setTimeout(function () {
     displayText.innerText = ""
     button.style.visibility = 'visible';
   }, 4800);
 
-
-  setTimeout(function(){
+  setTimeout(function () {
     displayText.innerText = "END"
   }, 180000);
 
